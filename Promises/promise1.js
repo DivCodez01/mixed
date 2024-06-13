8d44a3f26d21b4d4ffcc77a34a2198432511d9a4
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-13 19:13:08 +0200

    promise meaning

---
+++ Promises/promise1.js
@@ -0,0 +1,11 @@
+// Promise = is an Object that manages asynchronous operations.
+//           wrap a Promise Object around {asynchronous code}
+//           "I promise to return a value"
+//           PENDING -> RESOLVED or REJECTED
+//           new Promise((resolve, reject) => {asynchronous code})
+
+// DO THESE CHORES IN ORDER
+
+// 1. WALK THE DOG
+// 2. cLEAN THE KITCHEN
+// 3. TAKE OUT THE TRASH
