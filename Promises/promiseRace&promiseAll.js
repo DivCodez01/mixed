1dcc9526bfee321d6eb578df38f28de0cd4ba350
divin-gakiza <divin.gakiza01@gmail.com>
2024-06-13 20:34:39 +0200

    promise race and promise all in promise

---
+++ Promises/promiseRace&promiseAll.js
@@ -0,0 +1,11 @@
+const recoreVideoOne = new Promise((resolve, reject) => {
+    resolve('Video 1 Recorded')
+})
+
+const recoreVideoTwo = new Promise((resolve, reject) => {
+    resolve('Video 2 Recorded')
+})
+
+const recoreVideoThree = new Promise((resolve, reject) => {
+    resolve('Video 3 Recorded')
+})
